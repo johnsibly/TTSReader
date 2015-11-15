4580b2c20c0f3227c7cfcb824c5fc6532cd427e5
John Sibly <john@sibly.co.uk>
2015-11-15 22:04:49 +0000

    Initial commit

---
+++ TextToSpeechForm.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace TTSReader
+{
+    partial class TextToSpeechReaderForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TextToSpeechReaderForm));
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.labelPosition = new System.Windows.Forms.Label();
+            this.numericUpDownRate = new System.Windows.Forms.NumericUpDown();
+            this.buttonPause = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRate)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonPlay.Location = new System.Drawing.Point(16, 227);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(60, 23);
+            this.buttonPlay.TabIndex = 0;
+            this.buttonPlay.Text = "Play";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Text here:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(13, 30);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(459, 191);
+            this.textBox1.TabIndex = 2;
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonStop.Enabled = false;
+            this.buttonStop.Location = new System.Drawing.Point(148, 227);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(60, 23);
+            this.buttonStop.TabIndex = 3;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // labelPosition
+            // 
+            this.labelPosition.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelPosition.AutoSize = true;
+            this.labelPosition.Location = new System.Drawing.Point(402, 232);
+            this.labelPosition.Name = "labelPosition";
+            this.labelPosition.Size = new System.Drawing.Size(70, 13);
+            this.labelPosition.TabIndex = 4;
+            this.labelPosition.Text = "00:00:00.000";
+            // 
+            // numericUpDownRate
+            // 
+            this.numericUpDownRate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.numericUpDownRate.Location = new System.Drawing.Point(340, 230);
+            this.numericUpDownRate.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDownRate.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDownRate.Name = "numericUpDownRate";
+            this.numericUpDownRate.Size = new System.Drawing.Size(40, 20);
+            this.numericUpDownRate.TabIndex = 5;
+            this.numericUpDownRate.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // buttonPause
+            // 
+            this.buttonPause.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonPause.Enabled = false;
+            this.buttonPause.Location = new System.Drawing.Point(82, 227);
+            this.buttonPause.Name = "buttonPause";
+            this.buttonPause.Size = new System.Drawing.Size(60, 23);
+            this.buttonPause.TabIndex = 6;
+            this.buttonPause.Text = "Pause";
+            this.buttonPause.UseVisualStyleBackColor = true;
+            this.buttonPause.Click += new System.EventHandler(this.buttonPause_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(208, 232);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Playback rate (-10 to 10):";
+            // 
+            // TextToSpeechReaderForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 262);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonPause);
+            this.Controls.Add(this.numericUpDownRate);
+            this.Controls.Add(this.labelPosition);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonPlay);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(500, 300);
+            this.Name = "TextToSpeechReaderForm";
+            this.Text = "Text to Speech Reader";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRate)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Label labelPosition;
+        private System.Windows.Forms.NumericUpDown numericUpDownRate;
+        private System.Windows.Forms.Button buttonPause;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TextToSpeechForm.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Diagnostics;
+using System.Speech.Recognition;
+
+namespace TTSReader
+{
+    public partial class TextToSpeechReaderForm : Form
+    {
+        private enum State
+        {
+            Stopped,
+            Playing,
+            Paused
+        }
+
+        private SpeechSynthesizer _synthesizer = new SpeechSynthesizer();
+        private State _state = State.Stopped;
+
+        private void UpdateState(State newState)
+        {
+            _state = newState;
+            buttonPlay.Enabled = _state != State.Playing;
+            buttonPause.Enabled = _state == State.Playing;
+            buttonStop.Enabled = _state != State.Stopped;
+        }
+
+        public TextToSpeechReaderForm()
+        {
+            InitializeComponent();
+
+            _synthesizer.SpeakCompleted += new EventHandler<SpeakCompletedEventArgs>(_synthesizer_SpeakCompleted);
+            _synthesizer.PhonemeReached += new EventHandler<PhonemeReachedEventArgs>(_synthesizer_PhonemeReached);
+            _synthesizer.SpeakProgress += new EventHandler<SpeakProgressEventArgs>(_synthesizer_SpeakProgress);
+            _synthesizer.StateChanged += new EventHandler<System.Speech.Synthesis.StateChangedEventArgs>(_synthesizer_StateChanged);
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            if (_state == State.Stopped)
+            {
+                _synthesizer.SpeakAsync(textBox1.Text);
+            }
+            else
+            {
+                _synthesizer.Resume();
+            }
+            UpdateState(State.Playing);
+        }
+
+        void _synthesizer_StateChanged(object sender, System.Speech.Synthesis.StateChangedEventArgs e)
+        {
+            Debug.WriteLine("StateChanged " + e.State.ToString());
+        }
+
+        void _synthesizer_SpeakProgress(object sender, SpeakProgressEventArgs e)
+        {
+            textBox1.Select(e.CharacterPosition, e.CharacterCount);
+        }
+
+        void _synthesizer_PhonemeReached(object sender, PhonemeReachedEventArgs e)
+        {
+            labelPosition.Text = string.Format("{0}:{1}:{2}.{3}", 
+                e.AudioPosition.Hours.ToString("00"), 
+                e.AudioPosition.Minutes.ToString("00"), 
+                e.AudioPosition.Seconds.ToString("00"), 
+                e.AudioPosition.Milliseconds.ToString("000"));
+        }
+
+        void _synthesizer_SpeakCompleted(object sender, SpeakCompletedEventArgs e)
+        {
+            textBox1.Select(0, 0);
+            UpdateState(State.Stopped);
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            switch (_state)
+            {
+            case State.Playing:
+                _synthesizer.SpeakAsyncCancelAll();
+                break;
+            default:
+                break;
+            }
+            UpdateState(State.Stopped);
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            _synthesizer.Rate = (int)numericUpDownRate.Value;
+        }
+
+        private void buttonPause_Click(object sender, EventArgs e)
+        {
+            _synthesizer.Pause();
+            UpdateState(State.Paused);
+        }
+
+        void _recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            textBox1.Text.Insert(textBox1.SelectionStart, e.Result.Text);
+        }
+    }
+}
